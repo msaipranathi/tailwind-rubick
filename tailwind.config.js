ae6b8eb5c615e1927f46065114e4021e31582549
msaipranathi <45840010+msaipranathi@users.noreply.github.com>
2021-06-03 10:10:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+      screens: {
+        'xs': '360px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+
+      
+    },
+  
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
